2eef1c82885189a70e04842b23e63e666959ae25
Irina Kulakova <irina.kulakova.ru@mail.ru>
2022-01-17 01:56:09 +0300

    creat page - correct users

--- depending/js/admin_main.js
+++ depending/js/admin_main.js
@@ -1,3 +1,11 @@
 function logoClick() {
     document.location.href = "admin_main.html";
   }
+
+function correctUsers() {
+  document.location.href = "correct_users.html";
+}  
+
+function deleteTamagotchi() {
+  alert('Delete?');
+}
