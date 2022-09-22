97a998daf7eb38eb61fe74fd73a03dddd2657455
Irina <irina.kulakova.ru@mail.ru>
2022-09-22 16:01:07 +0300

    add POST-method

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,21 +1,71 @@
 import React, { useState } from 'react';
 import { Button } from "react-bootstrap";
 import s from "./PostForm.module.css";
+import axios from "axios";
 
 const PostForm = ( {creat}) => {
-    const [post, setPost] = useState({title: '', description: ''});
+    const [post, setPost] = useState({title: '', text: '', id: 0, url: ''});    
 
-    const addNewPost = (e) => {
+    const addNewPost =  (e) => {
         e.preventDefault();
         const newPost = {
-            ...post, 
-            id: Date.now(), 
-            url: 'https://detiplanet.ru/upload/iblock/0c6/0c6928bda57395aa80c4e4d14eac080e.png',
-            deeplink: '',
-        }
+            ...post,
+            id: Date.now()
+        }        
         creat(newPost);
-        setPost({title: '', description: ''});
+        sendPost(post);
+        setPost({title: '', text: '', id: 0, url: ''});
     }
+
+    // const addNewPost = async (e) => {
+        
+    //     e.preventDefault();
+    //     try {
+    //         const auth = await axios.post("https://tamagotchi-backend.herokuapp.com/user/login", {
+    //             login: "string",
+    //             password: "string",
+    //         });
+
+    //         const response = await axios.post(
+    //             "https://tamagotchi-backend.herokuapp.com/image_data/",
+    //             { title: post.title, text: post.text, url: post.url },
+    //             {
+    //                 headers: {
+    //                     Authorization: `Bearer ${auth.data.access_token}`,
+    //                 },
+    //             }
+    //         );
+    //         console.log(response.data);            
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    //     creat(post);
+    //     console.log(post.id);
+    //     setPost({title: '', text: '', id: 0, url: ''});
+    // };
+    
+
+    const sendPost = async (posts) => {
+        try {
+            const auth = await axios.post("https://tamagotchi-backend.herokuapp.com/user/login", {
+                login: "string",
+                password: "string",
+            });
+
+            const response = await axios.post(
+                "https://tamagotchi-backend.herokuapp.com/image_data/",
+                { title: posts.title, text: posts.text, url: posts.url },
+                {
+                    headers: {
+                        Authorization: `Bearer ${auth.data.access_token}`,
+                    },
+                }
+            );
+            console.log(response.data);            
+        } catch (err) {
+            console.log(err);
+        }               
+    };
 
     return (
         <form>
@@ -28,8 +78,14 @@ const PostForm = ( {creat}) => {
             <input 
                 type='text'
                 placeholder='Описание поста'
-                value={post.description}
-                onChange={e => setPost({...post, description: e.target.value })}
+                value={post.text}
+                onChange={e => setPost({...post, text: e.target.value })}
+            />
+            <input 
+                type="text"
+                placeholder='Ваша картинка'
+                value={post.url}
+                onChange={e => setPost({...post, url: e.target.value})}
             />
             <Button onClick={addNewPost} className={s.button__form}>Создать новость</Button>            
         </form>
